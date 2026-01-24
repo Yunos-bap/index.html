0b5427db01dc9863aa6e47e86352c7d1e9e2840b
Yunos-bap <gryttenholmt@gmail.com>
2026-01-24 14:13:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const speed = 6;
 // PLATTFORM-INNSTILLINGER (Må være like som i HTML/CSS)
 // I din index.html står det: left: 100px, bottom: 80px, width: 150px
 const platX = 100;
-const platY = -60;  // Vi bruker minus fordi pY går nedover fra bakken
+const platY = -80;  // Vi bruker minus fordi pY går nedover fra bakken
 const platWidth = 150;
 
 function update() {
